1fbd847fff8061a397ca72e52487d4e3184b7d45
BertrandHustle <MFOrangeSlice@gmail.com>
2017-03-07 17:42:11 -0500

    added files for testing and GUI

--- goodsetter.py
+++ goodsetter.py
@@ -1,16 +1,21 @@
 import sys
 import os
+import re
 
 args = sys.argv
 
 if len(args) != 2:
-    print ("Usage: goodsetter <directory>")
+    print ('Usage: goodsetter <directory>'')
 else:
     directory_path = args[1]
     os.chdir(directory_path)
     directory = os.listdir(directory_path)
+    # unzip all the zip files
     for file in directory:
-        file = ("\""+file+"\"")
+        file = ('\''+file+'\'')
         os.system('7z e ' + file)
-
-        #add handling for .7z w/multiple files
+        # check for beta/weird versions of roms and get rid of them
+        # we want to do this for each zip file, that way we won't be traversing the whole directory twice
+        for file in directory:
+            if not file.endswith('.zip') and re.search('[*]', file):
+                os.system('del ' + file)
